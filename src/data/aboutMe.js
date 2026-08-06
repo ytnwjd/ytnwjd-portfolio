c8e1b32bec4c11087b93c3d4b89415b7b0e02cc3
ysj120808 <ysj120808@gmail.com>
2026-08-06 17:13:43 +0900

    feat: add aboutMe data

---
+++ src/data/aboutMe.js
@@ -0,0 +1,43 @@
+export const aboutMe = [
+  {
+    emoji: '📚',
+    fragments: [
+      { text: '더 나은 효율을 위해, ', highlight: false },
+      { text: '끊임없이 새로운 기술을 학습하고 도전', highlight: true },
+      { text: '합니다!', highlight: false },
+    ],
+  },
+  {
+    emoji: '🏋️‍♀️',
+    fragments: [
+      { text: '꾸준한 운동으로 다진 탄탄한 체력을 통해, ', highlight: false },
+      { text: '지치지 않는 끈기를', highlight: true },
+      { text: ' 만들어냅니다!', highlight: false },
+    ],
+  },
+  {
+    emoji: '💑',
+    fragments: [
+      { text: '기록하고 공유하며, ', highlight: false },
+      { text: '동료와 함께 성장하는 가치', highlight: true },
+      { text: '를 믿습니다!', highlight: false },
+    ],
+  },
+  {
+    emoji: '⛓️',
+    fragments: [
+      { text: '작은 디테일이 모여, 견고한 시스템을', highlight: true },
+      { text: ' 만든다고 생각합니다!', highlight: false },
+    ],
+  },
+  {
+    emoji: '🔥',
+    fragments: [
+      { text: '조직의 ', highlight: false },
+      { text: '목표를 나의 성취로 여기는 소속감', highlight: true },
+      { text: '을, 매 순간 쏟는 ', highlight: false },
+      { text: '열정으로 치환', highlight: true },
+      { text: '합니다!', highlight: false },
+    ],
+  },
+]
