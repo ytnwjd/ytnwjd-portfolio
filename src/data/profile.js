e1d829ac17847a7582f57f96da5526a01bf02a4f
ysj120808 <ysj120808@gmail.com>
2026-08-06 13:36:50 +0900

    chore: add profile data

---
+++ src/data/profile.js
@@ -0,0 +1,8 @@
+export const profile = {
+  birth: '2003.12.08',
+  university: '아주대학교 소프트웨어학과',
+  universityPeriod: '22.03 ~ 26.08',
+  email: 'ysj120808@gmail.com',
+  github: 'https://github.com/ytnwjd',
+  blog: 'https://ytnwjd.tistory.com/',
+}
