fea6565af5724ee80a15df3df78c199ac90ed202
ysj120808 <ysj120808@gmail.com>
2026-09-16 11:40:50 +0900

    feat: add TopNav and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Sidebar from '@/components/layout/Sidebar.vue'
+import TopNav from '@/components/layout/TopNav.vue'
+import Footer from '@/components/layout/Footer.vue'
 </script>
 
 <template>
   <div class="layout">
-    <Sidebar />
+    <TopNav />
     <main class="content">
       <RouterView />
     </main>
+    <Footer />
   </div>
 </template>
 
 <style scoped>
 .layout {
   display: flex;
-  /* 사이드바가 stretch되면 position: sticky가 먹히지 않으므로 자기 높이(100vh)를 유지시킴 */
-  align-items: flex-start;
+  flex-direction: column;
+  min-height: 100%;
 }
 
 .content {
