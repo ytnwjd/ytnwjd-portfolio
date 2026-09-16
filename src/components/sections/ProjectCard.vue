fb28fda5f3d89380f75a9c4694b50883ebae27ea
ysj120808 <ysj120808@gmail.com>
2026-09-16 14:56:56 +0900

    fix: add stdard line-clamp property

--- src/components/sections/ProjectCard.vue
+++ src/components/sections/ProjectCard.vue
@@ -159,6 +159,7 @@ function tagStyle(tech) {
   color: var(--color-text-muted);
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
   word-break: keep-all;
