cec8149a86800e2ae839ec76f33bc2f46e969635
ysj120808 <ysj120808@gmail.com>
2026-09-17 15:00:32 +0900

    chore: remove unused pinia dependency

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -13,7 +12,6 @@ if ('scrollRestoration' in history) {
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
